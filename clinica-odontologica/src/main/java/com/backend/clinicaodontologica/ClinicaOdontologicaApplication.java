39a41cd85f1d47be37aa0962026649addd4b6d9e
JhonatanRios <jhoto1234@gmail.com>
2023-11-09 19:47:31 -0500

    Eliminar proyecto y recomensar

--- clinica-odontologica/src/main/java/com/backend/clinicaodontologica/ClinicaOdontologicaApplication.java
+++ clinica-odontologica/src/main/java/com/backend/clinicaodontologica/ClinicaOdontologicaApplication.java
@@ -1,13 +1,24 @@
 package com.backend.clinicaodontologica;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class ClinicaOdontologicaApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(ClinicaOdontologicaApplication.class);
 	public static void main(String[] args) {
 		SpringApplication.run(ClinicaOdontologicaApplication.class, args);
+		logger.info("ClinicaOdontologica is now running...");
 	}
 
+	@GetMapping
+	public String holaCamada(){
+		return "Hola sofi";
+	}
 }
