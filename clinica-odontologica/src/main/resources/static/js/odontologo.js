96260b31d80e365070ad58632d1327d52b68a56a
Sofi Brugo <brugosofi@gmail.com>
2023-11-24 21:05:08 -0300

    odontologos front

---
+++ clinica-odontologica/src/main/resources/static/js/odontologo.js
@@ -0,0 +1,50 @@
+const opciones = [
+    {
+      imagen: './assets/registro.png',
+      tipo: 'Registrar odontólogo',
+      link: './odontologo.html',
+      descrip: 'Aqui podras registrar, listar, buscar por id, actualizar y eliminar registros'
+    },
+    {
+      imagen: './assets/lista.png',
+      tipo: 'Listar odontólogos',
+      link: './odontologo.html',
+      descrip: 'Aqui podras registrar odontólogos'
+    },
+    {
+      imagen: './assets/turno.png',
+      tipo: 'Buscar por Id',
+      link: './turno.html',
+      descrip: 'Aqui podras buscar odontólogos por id'
+    },
+    {
+        imagen: './assets/turno.png',
+        tipo: 'Actualizar y eliminar registros',
+        link: './turno.html',
+        descrip: 'Aqui podras actualizar y eliminar registros de odontólogos'
+      }
+  ]
+  
+  const contCards = document.querySelector('.cont-card');
+  
+  const cargarCardOpciones = () => {
+    contCards.innerHTML = '';
+    opciones.forEach(function (opcion) {
+      let card = document.createElement('a');
+      card.classList.add('d-grid', 'center', 'card', 'box', 'g--10', 'p--15');
+      card.href = opcion.link;
+  
+      card.innerHTML = `
+        <img class="img--entidad" src="${opcion.imagen}" alt="Imagen de ${opcion.tipo}">
+        <p class="txt--center txt--gray">${opcion.tipo} <br> <span>${opcion.descrip}</span></p>
+      `;
+  
+      contCards.appendChild(card);
+    })
+  };
+  
+  cargarCardOpciones();
+  
+  console.log(contCards);
+  
+  
