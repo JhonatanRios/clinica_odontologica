9de2a6983262e857c2d1d6d42ad0bb812fcb3243
Sofi Brugo <brugosofi@gmail.com>
2023-11-29 15:28:34 -0300

    Test Class Paciente Service

--- clinica-odontologica/src/main/java/com/backend/clinicaodontologica/test/OdontologoServiceTest.java
+++
@@ -1,8 +0,0 @@
-package com.backend.clinicaodontologica.test;
-
-import com.backend.clinicaodontologica.service.impl.OdontologoService;
-
-public class OdontologoServiceTest {
-    //private final OdontologoService odontologoService = new OdontologoService(new OdontologoDaoH2());
-
-}
---
+++ clinica-odontologica/src/test/java/com/backend/clinicaodontologica/test/OdontologoServiceTest.java
@@ -0,0 +1,15 @@
+package com.backend.clinicaodontologica.test;
+
+import com.backend.clinicaodontologica.service.impl.OdontologoService;
+import com.backend.clinicaodontologica.service.impl.PacienteService;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class PacienteServiceTest {
+    @Autowired
+    private PacienteService pacienteService;
+    @Test
+
+}
